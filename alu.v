e9f86fd01380c3abf623802a249b16fde98a2709
SravaniMende0706 <n200640@rguktn.ac.in>
2026-01-21 18:20:11 +0530

    alu.v

---
+++ alu.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module alu (
+    input wire [31:0] a,
+    input wire [31:0] b,
+    input wire [3:0] alu_ctrl,
+    output reg [31:0] result,
+    output wire zero
+);
+    always @(*) begin
+        case (alu_ctrl)
+            4'b0000: result = a + b;
+            4'b0001: result = a - b;
+            4'b0010: result = a & b;
+            4'b0011: result = a | b;
+            4'b0100: result = a ^ b;
+            4'b0101: result = ($signed(a) < $signed(b));
+            4'b0110: result = (a < b);
+            4'b0111: result = a << b[4:0];
+            4'b1000: result = a >> b[4:0];
+            4'b1001: result = $signed(a) >>> b[4:0];
+            default: result = 32'b0;
+        endcase
+    end
+    assign zero = (result == 32'b0);
+endmodule
+
